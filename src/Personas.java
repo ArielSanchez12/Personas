7063b5eaac30c385f3b9941a76dd625b3cf574ac
ArielSanchez12 <riveraariel433@gmail.com>
2024-10-10 10:01:05 -0500

    mmm raro

--- src/Personas.java
+++ src/Personas.java
@@ -1,15 +1,40 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Personas {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    String nombre,direccion, ocupacion;
+    int edad, telefono;
+
+    //metodo contruktor para inicializar las variables
+    public Personas(String nombre, String direccion, String ocupacion, int edad, int telefono){
+        this.nombre = nombre; //Por k se pone this????????
+        this.direccion = direccion;
+        this.ocupacion = ocupacion;
+        this.edad = edad;
+        this.telefono = telefono;
+    }
+    public void presentarse(){
+        System.out.println("Hola mi nombre es " + nombre + " mi direccion es: "+ direccion+" mi ocupacion es: "+ocupacion+", mi edad es"+edad+" mi telefono es: "+telefono);
+    }
+    public void correr(){
+        System.out.println("La persona " + nombre + " corre por " + direccion+ " aparenta tener " + edad);
+    }
+
+    public static void main(String[] args) {
+
+        Personas estudiante = new Personas("Diego", "San Jose de Moran", "Estudiante",  20, 978852305);
+        Personas docente = new Personas("Monica", "La Floresta", "Docente",  99, 978850523);
+        Personas secretaria = new Personas("Marta", "Esfot", "Secretaria",  29, 987456532);
+
+        estudiante.presentarse(); //llamada al metodo presentarse()
+        docente.presentarse();
+        secretaria.presentarse();
+
+        System.out.println();
+
+        estudiante.correr(); //llamada al metodo correr()
+        docente.correr();
+        secretaria.correr();
+
     }
 }
